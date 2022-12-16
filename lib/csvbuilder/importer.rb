23e824f7a095aa6713bafd3e7cb7357829e173f6
Joel Azemar <joel.azemar@gmail.com>
2022-12-16 13:57:45 +0100

    Initial Commit

--- lib/csvbuilder/importer.rb
+++ lib/csvbuilder/importer.rb
@@ -13,22 +13,6 @@ require "active_support/core_ext/string"
 
 require "csvbuilder/core"
 
-# require "csvbuilder/core/version"
-# puts("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! => #{::Csvbuilder::Core::VERSION}")
-
-# require "csvbuilder/core/public/model"
-# ::Csvbuilder::Model
-
-# require "csvbuilder/core/concerns/attributes_base"
-# ::Csvbuilder::AttributesBase
-
-# require "csvbuilder/core/internal/attribute_base"
-# ::Csvbuilder::AttributeBase
-
-# require "csvbuilder/importer/model"
-# require "model"
-# require "file_model"
-
 require "csvbuilder/importer/public/import"
 require "csvbuilder/importer/public/import/file_model"
 require "csvbuilder/importer/public/import/file"
